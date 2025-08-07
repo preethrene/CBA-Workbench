f7a7158898374a5579c4453839cdb1b6bf21875a
Preetham N <151754260+preethrene@users.noreply.github.com>
2025-08-07 19:13:13 +0530

    Add files via upload

---
+++ Express js/app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
